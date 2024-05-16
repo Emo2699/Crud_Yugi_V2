a01f4ff98977225ab3670cca0f781a8b21d04a78
yoshua200 <yoshua200@gmail.com>
2024-05-16 11:01:04 -0600

    Trabajo Andre

--- src/main/java/yugiV2/yugiV2/repository/v1/MonstruoDAO.java
+++ src/main/java/yugiV2/yugiV2/repository/v1/MonstruoDAO.java
@@ -1,12 +1,18 @@
 package yugiV2.yugiV2.repository.v1;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import yugiV2.yugiV2.models.v1.Monstruo;
 
+import java.util.List;
+
 
 @Repository
-public interface MonstruoDAO extends CrudRepository<Monstruo,String> {
+public interface MonstruoDAO extends JpaRepository<Monstruo,String> {
+    public List <Monstruo> findByNombreIgnoringCaseContaining(String nombre);
+
 
 
 }
